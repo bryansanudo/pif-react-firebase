6e32433b447d2881b08d6bb78c2169f2bd86e3cc
Bryan <bryansanudo95@gmail.com>
2023-05-01 16:20:18 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "@/components/Header";
+import Home from "@/pages/Home";
+import Contact from "@/pages/Contact";
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
